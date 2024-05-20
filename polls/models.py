c179ecd780d96d7b64626679ee43e3261bdd0743
implaymo <goncalomorerasilva@gmail.com>
2024-05-20 20:07:00 +0100

    Manage to store all products in database. Need fixing.

--- polls/models.py
+++ polls/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField()
+    description = models.CharField(max_length=100)
+    image = models.CharField(max_length=100)
+        
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render 
 from api import Api
+from polls.models import Product
 
 def index(request):
+    api = Api()
+    api.get_all_products()
+
+    try:
+        for dictionary in api.all_products:
+            Product.objects.create(name=dictionary["name"], price=dictionary["price"], description=dictionary["description"], image=dictionary["image"])
+    except Exception as e:
+        print(f"ERROR: {e}")
+    
     return render(request, "index.html")
