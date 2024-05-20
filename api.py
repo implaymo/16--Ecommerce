054d9b6e67aa3e834737411a860d6e60e0125b29
implaymo <goncalomorerasilva@gmail.com>
2024-05-20 15:00:01 +0100

    Manage to get products from api and store in a list of dictionaries.

---
+++ api.py
@@ -0,0 +1,31 @@
+import requests
+import json
+
+class Api():
+    def __init__(self) -> None:
+        self.url = "https://dummyjson.com/products"
+        self.all_products = []
+        
+
+    def get_all_products(self):
+        response = requests.get(self.url)
+        data = response.json()
+        total_products = data["products"]
+
+
+        for i in range(len(total_products)):
+            product_name = data["products"][i]["title"]
+            product_price = data["products"][i]["price"]
+            product_description = data["products"][i]["description"]
+            product_image = data["products"][i]["thumbnail"]
+            
+            if product_name in self.all_products:
+                continue
+            else:
+                self.all_products.append({
+                    "name": product_name,
+                    "price": product_price,
+                    "description": product_description,
+                    "image": product_image,
+                })
+
--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render 
-
+from api import Api
 
 def index(request):
+    api = Api()
+
+    api.get_all_products()
+    
     return render(request, "index.html")
