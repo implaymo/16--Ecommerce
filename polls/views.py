53d2a78bc389ac773f732200954fcb5737019d23
implaymo <goncalomorerasilva@gmail.com>
2024-05-22 10:18:59 +0100

    Fix compare_api_db_data().

--- polls/views.py
+++ polls/views.py
@@ -16,12 +16,11 @@ def get_db_data():
         all_db_data.append(product.name)
 
 def compare_api_db_data():
-    for dictionary in api.db_data:
+    for dictionary in api.all_products:
         if dictionary["name"] in all_db_data:
             continue
         else:
             update_db(name=dictionary["name"], price=dictionary["price"], description=dictionary["description"], image=dictionary["image"])
-
 
 def index(request):
     try: 
